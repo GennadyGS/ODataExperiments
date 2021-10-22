2138fcb4f9c9d494a07f6f583646b6a3d4391fed
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-10-22 18:44:56 +0300

    Enable ODATA

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
 
 namespace ODataExperiments.Controllers
 {
@@ -8,8 +9,8 @@ namespace ODataExperiments.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+           "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -19,6 +20,7 @@ namespace ODataExperiments.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
+        [EnableQuery]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
