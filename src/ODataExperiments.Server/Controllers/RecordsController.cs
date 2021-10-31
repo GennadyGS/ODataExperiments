33b3b0f35f91049849a8e0dc0bc791a580510426
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-10-31 17:24:34 +0200

    Extract class RecordsProvider

--- src/ODataExperiments.Server/Controllers/RecordsController.cs
+++ src/ODataExperiments.Server/Controllers/RecordsController.cs
@@ -5,13 +5,6 @@ namespace ODataExperiments.Server.Controllers;
 
 public class RecordsController : ODataController
 {
-    private const int RecordCount = 1000;
-    private const int FieldCount = 200;
-
-    private static IReadOnlyCollection<Record> Records { get; } = 
-        RecordsGenerator.GenerateRecords(RecordCount, FieldCount);
-
     [EnableQuery]
-    public IReadOnlyCollection<Record> GetRecords() => Records;
-
+    public IReadOnlyCollection<Record> GetRecords() => RecordsProvider.Records;
 }
---
+++ src/ODataExperiments.Server/RecordsProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace ODataExperiments.Server
+{
+    internal static class RecordsProvider
+    {
+        private const int RecordCount = 1000;
+        private const int FieldCount = 200;
+
+        public static IReadOnlyCollection<Record> Records { get; } =
+            RecordsGenerator.GenerateRecords(RecordCount, FieldCount);
+    }
+}
