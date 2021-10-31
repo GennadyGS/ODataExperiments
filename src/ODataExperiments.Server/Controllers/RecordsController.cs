e362e829dfacc15b0f995b500c4babe7ec065f1e
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-10-31 17:19:50 +0200

    refactor: extract class RecordsGenerator

--- src/ODataExperiments.Server/Controllers/RecordsController.cs
+++ src/ODataExperiments.Server/Controllers/RecordsController.cs
@@ -1,5 +1,4 @@
-﻿using Bogus;
-using Microsoft.AspNetCore.OData.Query;
+﻿using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
 
 namespace ODataExperiments.Server.Controllers;
@@ -8,51 +7,11 @@ public class RecordsController : ODataController
 {
     private const int RecordCount = 1000;
     private const int FieldCount = 200;
-    private const int Seed = 6734345;
-    private const int MaxStringLength = 31;
-
-    static RecordsController() =>
-        Randomizer.Seed = new Random(Seed);
 
     private static IReadOnlyCollection<Record> Records { get; } = 
-        GenerateRecords();
+        RecordsGenerator.GenerateRecords(RecordCount, FieldCount);
 
     [EnableQuery]
     public IReadOnlyCollection<Record> GetRecords() => Records;
 
-    private static IReadOnlyCollection<Record> GenerateRecords() =>
-        new Faker<Record>()
-            .CustomInstantiator(GenerateRecord)
-            .Generate(RecordCount);
-
-    private static Record GenerateRecord(Faker faker) =>
-        new()
-        {
-            Id = faker.IndexFaker,
-            Properties = GenerateProperties(faker),
-        };
-
-    private static IDictionary<string, object> GenerateProperties(Faker faker) =>
-        Enumerable.Range(0, FieldCount)
-            .ToDictionary(
-                i => $"Field_{i:D3}",
-                _ => GeneratePropertyValue(faker));
-
-    private static object GeneratePropertyValue(Faker faker) =>
-        faker.Random.Enum<FieldType>() switch
-        {
-            FieldType.Bool => faker.Random.Bool(),
-            FieldType.Char => faker.Random.Char('0', '9'),
-            FieldType.String => faker.Random.AlphaNumeric(faker.Random.Int(0, MaxStringLength)),
-            FieldType.Decimal => faker.Random.Decimal(),
-            _ => throw new InvalidOperationException("Unknown type"),
-        };
-
-    private enum FieldType
-    {
-        Bool,
-        Char,
-        String,
-        Decimal,
-    }
 }
---
+++ src/ODataExperiments.Server/RecordsGenerator.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+
+namespace ODataExperiments.Server;
+
+internal static class RecordsGenerator
+{
+    private const int MaxStringLength = 31;
+
+    private const int Seed = 6734345;
+
+    static RecordsGenerator() =>
+        Randomizer.Seed = new Random(Seed);
+
+    public static IReadOnlyCollection<Record> GenerateRecords(
+        int recordCount, int fieldCount) =>
+        new Faker<Record>()
+            .CustomInstantiator(faker => GenerateRecord(faker, fieldCount))
+            .Generate(recordCount);
+
+    private static Record GenerateRecord(Faker faker, int fieldCount) =>
+        new()
+        {
+            Id = faker.IndexFaker,
+            Properties = GenerateProperties(faker, fieldCount),
+        };
+
+    private static IDictionary<string, object> GenerateProperties(
+        Faker faker, int fieldCount) =>
+        Enumerable.Range(0, fieldCount)
+            .ToDictionary(
+                i => $"Field_{i:D3}",
+                _ => GeneratePropertyValue(faker));
+
+    private static object GeneratePropertyValue(Faker faker) =>
+        faker.Random.Enum<FieldType>() switch
+        {
+            FieldType.Bool => faker.Random.Bool(),
+            FieldType.Char => faker.Random.Char('0', '9'),
+            FieldType.String => faker.Random.AlphaNumeric(faker.Random.Int(0, MaxStringLength)),
+            FieldType.Decimal => faker.Random.Decimal(),
+            _ => throw new InvalidOperationException("Unknown type"),
+        };
+
+    private enum FieldType
+    {
+        Bool,
+        Char,
+        String,
+        Decimal,
+    }
+
+}
