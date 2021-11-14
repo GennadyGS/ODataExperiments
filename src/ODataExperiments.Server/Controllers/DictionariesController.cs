5f90a624b2a05938032ce76c9b2fceb80dd28309
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-11-14 15:52:11 +0200

    Add missing attribute

--- src/ODataExperiments.Server/Controllers/DictionariesController.cs
+++ src/ODataExperiments.Server/Controllers/DictionariesController.cs
@@ -5,6 +5,7 @@ namespace ODataExperiments.Server.Controllers;
 [Route("[controller]")]
 public sealed class DictionariesController : Controller
 {
+    [HttpGet]
     public IReadOnlyCollection<IDictionary<string, object>> GetDictionaries() => 
         RecordsProvider.Dictionariess;
 }
