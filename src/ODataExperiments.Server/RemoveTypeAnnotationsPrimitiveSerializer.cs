70a765dd0232f7a210ae9999a7d2cf437a3264b4
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-04-27 18:48:19 +0300

    Adjust code style

--- src/ODataExperiments.Server/RemoveTypeAnnotationsPrimitiveSerializer.cs
+++ src/ODataExperiments.Server/RemoveTypeAnnotationsPrimitiveSerializer.cs
@@ -7,7 +7,8 @@ namespace ODataExperiments.Server;
 internal sealed class RemoveTypeAnnotationsPrimitiveSerializer : ODataPrimitiveSerializer
 {
     public override ODataPrimitiveValue CreateODataPrimitiveValue(
-        object graph, IEdmPrimitiveTypeReference primitiveType,
+        object graph, 
+        IEdmPrimitiveTypeReference primitiveType,
         ODataSerializerContext writeContext)
     {
         var result = base.CreateODataPrimitiveValue(graph, primitiveType, writeContext);
