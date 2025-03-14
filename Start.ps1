b4066964cbb28266f8e0390b0216a29e776d4844
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-03-14 16:30:13 +0200

    Remove example for .NET 3.1

--- Start.ps1
+++ Start.ps1
@@ -3,7 +3,3 @@ taskkill.exe /IM dotnet.exe /t /f
 Start-Process "dotnet.exe" "watch" `
     -WorkingDirectory "$PSScriptRoot\src\ODataExperiments.Server" `
     -WindowStyle minimized
-
-Start-Process "dotnet.exe" "watch" `
-    -WorkingDirectory "$PSScriptRoot\src\ODataExperiments.Server.31" `
-    -WindowStyle minimized
